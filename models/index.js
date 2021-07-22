1ba5fb8b2b6e01dc04a9a1c3d7dccc71563f6880
Ted Heikkila <ted.heikkila@gmail.com>
2021-07-22 17:53:47 -0500

    fixed typo

--- models/index.js
+++ models/index.js
@@ -16,17 +16,17 @@ Blog.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
+Blog.hasMany(Comment, {
+  foreignKey: 'blog_id',
+  onDelete: 'CASCADE'
+});
+
 Comment.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
 Comment.belongsTo(Blog, {
   foreignKey: 'blog_id'
-});
-
-Post.hasMany(Comment, {
-  foreignKey: 'blog_id',
-  onDelete: 'CASCADE'
 });
 
 module.exports = { User, Blog };
