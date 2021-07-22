0302cddec0800165eb72d6f2f76b96821b903e7d
Ted Heikkila <ted.heikkila@gmail.com>
2021-07-21 19:49:05 -0500

    seeded data, ran npm install for node modules

--- seeds/seed.js
+++ seeds/seed.js
@@ -12,7 +12,7 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of blogData) {
+  for (const blog of blogData) {
     await Blog.create({
       ...blog,
       user_id: users[Math.floor(Math.random() * users.length)].id,
