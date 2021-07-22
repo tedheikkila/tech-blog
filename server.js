da9e62f5917213f886e0ddeba7e0851c8b690659
Ted Heikkila <ted.heikkila@gmail.com>
2021-07-21 19:25:19 -0500

    updated views folders

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -37,5 +37,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('now listening on 3001'));
 });
