5677637db2737307889b04de70be8ed487ef2662
Ted Heikkila <ted.heikkila@gmail.com>
2021-07-23 15:49:21 -0500

    dev'ing userRoutes api

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,35 +1,108 @@
 const router = require('express').Router();
-const { Blog } = require('../../models');
+const { Blog, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all blogs and JOIN with user data
+    await Blog.findAll({
+      order: [['date_created', 'DESC']],
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+
+        {
+          model: Comment,
+          attributes: ['name'],
+        }
+      ],
+    }).then(blogData => res.json(blogData))
+    
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+        {
+          model: Comment,
+          attributes: ['name'],
+        }
+      ],
+    }).then(blogData => {
+      if(!blogData) {
+        res.status(404).json({message: "No blog found with that id"})
+        return
+      }
+      res.json(blogData)
+  
+    })
+    
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+    
+});
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newBlog = await Blog.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
+    await Blog.create({
+      name: req.body.name,
+      description: req.body.description,
+      user_id: req.session.user_id
+    })
+    
+    .then(newBlog => res.json(newBlog))
 
-    res.status(200).json(newBlog);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    await Blog.update({
+      name: req.body.name,
+      description: req.body.description,
+      user_id: req.session.user_id
+    })
+    
+    .then(newBlog => res.json(newBlog))
+
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const blogData = await Blog.destroy({
+    await Blog.destroy({
       where: {
         id: req.params.id,
-        user_id: req.session.user_id,
       },
-    });
-
-    if (!blogData) {
-      res.status(404).json({ message: 'No blog found with that id' });
-      return;
-    }
-
-    res.status(200).json(blogData);
+    })
+    
+    .then(blogData => {
+      if(!blogData) {
+        res.status(404).json({message: "No blog found with that id"})
+        return
+      }
+      res.json(blogData)
+  
+    })
   } catch (err) {
     res.status(500).json(err);
   }
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,61 +1,148 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Comment, Blog } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.post('/', async (req, res) => {
-  try {
-    const userData = await User.create(req.body);
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.status(200).json(userData);
+// gets /api/users
+router.get('/', (req, res) => {
+  // find User model and run findAll
+  User.findAll({
+      attributes: { exclude: ['password'] }
+  })
+    .then(userData => res.json(userData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
-  } catch (err) {
-    res.status(400).json(err);
-  }
 });
 
-router.post('/login', async (req, res) => {
-  try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+// gets /api/users/id
+router.get('/:id', (req, res) => {
+  User.findOne({
+      attributes: { exclude: ['password']},
+      where: {
+        id: req.params.id
+      },
+      include: [
+          {
+            model: Blog,
+          },
+          {
+            model: Comment,
+          }
+        ]
+  })
+    .then(userData => {
+      if (!userData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(userData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
+// post /api/users
+router.post('/', (req, res) => {
+  User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  })
+  .then(userData => {
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.name = userData.name;
+      req.session.loggedIn = true;
+  
+      res.json(userData);
+    });
+  });
+});
+
+// post /login
+router.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(userData => {
     if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+      res.status(400).json({ message: 'No user with that email address' });
       return;
     }
 
-    const validPassword = await userData.checkPassword(req.body.password);
+    const okPassword = userData.checkPassword(req.body.password);
 
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+    if (!okPassword) {
+      res.status(400).json({ message: 'Incorrect password' });
       return;
     }
 
     req.session.save(() => {
       req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      
+      req.session.name = userData.name;
+      req.session.loggedIn = true;
+
       res.json({ user: userData, message: 'You are now logged in' });
     });
-
-  } catch (err) {
-    res.status(400).json(err);
-  }
+  });
 });
 
+
 router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
+  if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
     });
-  } else {
+  }
+  else {
     res.status(404).end();
   }
 });
+
+// put /api/users/id
+router.put('/:id', withAuth, (req, res) => {
+  User.update(req.body, {
+      individualHooks: true,
+      where: {
+          id: req.params.id
+    }
+  })
+    .then(userData => {
+      if (!userData[0]) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(userData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// dete /api/users/id
+router.delete('/:id', withAuth, (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(userData => {
+      if (!userData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(userData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 
 module.exports = router;
